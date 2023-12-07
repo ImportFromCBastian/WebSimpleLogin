6178910a5823af45eb81ded44f4def27cc7ff5f5
ImportFromCBastian <sebshndz2001@gmail.com>
2023-12-07 19:03:34 -0300

    Fix(styles):improved styles for Login and SignUp

--- simple-web/src/components/Login.jsx
+++ simple-web/src/components/Login.jsx
@@ -7,9 +7,12 @@ export const Login = () => {
   
   const navigate = useNavigate()
 
+  
+
   const [credentials, setCredentials] = useState({
     username: "",
-    password: ""
+    password: "",
+    showPassword: false,
   })
 
   const handleChange = (e) => {
@@ -25,23 +28,35 @@ export const Login = () => {
 
   }
 
+  const handleShowPassword = () => {
+    setCredentials({
+      ...credentials,
+      showPassword: !credentials.showPassword
+    })    
+  }
+
   
 
   return(
     <main>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Username</legend>
-          <input type="text" name="username" placeholder="karthuspentakill" value={credentials.username} onChange={handleChange} autoComplete="off"/>
-        </fieldset>
-        <fieldset>
-          <legend>Password</legend>
-          <input type="password" name="password" placeholder="Type your password" value={credentials.password} minLength={8} onChange={handleChange}/>
-        </fieldset>
-        <span>
-          <button type="submit">Sign In</button>
-          <Link to="/sign-up"> <button >Sign Up</button> </Link>
-        </span>
+      <form className="my-5 absolute"onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center border border-black rounded w-fit px-5 absolute">
+          <fieldset className="border border-black p-3 rounded my-4">
+            <legend>Username</legend>
+            <input type="text" name="username" placeholder="karthuspentakill" value={credentials.username} onChange={handleChange} autoComplete="off required"/>
+          </fieldset>
+          <fieldset className="flex border border-black p-3 rounded">
+            <legend>Password</legend>
+            <input type={ credentials.showPassword ? "text" : "password" } name="password" placeholder="Type your password" value={credentials.password} minLength={8} onChange={handleChange} required/>
+            { credentials.showPassword ? 
+            <button type="button" className="material-symbols-outlined" onClick={ handleShowPassword } >visibility</button> : 
+            <button type="button" className="material-symbols-outlined" onClick={ handleShowPassword } >visibility_off</button> }
+          </fieldset>
+          <span className="flex gap-5 my-3">
+            <button type="submit">Sign In</button>
+            <Link to="/sign-up"> <button >Sign Up</button> </Link>
+          </span>
+        </div>
       </form>
     </main>
   )
--- simple-web/src/components/SignUp.jsx
+++ simple-web/src/components/SignUp.jsx
@@ -3,42 +3,42 @@ import { Link } from "react-router-dom"
 export const SignUp = () => {
 
   return(
-    <div className="">
-      <form className="flex items-center mx-8 border-2 rounded-xl border-red-500 flex-col mt-6 min-h-[500] max-h-[890]">
-        <fieldset className="border border-black p-3 rounded">
+    <form className="my-5">
+      <div className="flex flex-col items-center border border-black rounded w-fit px-5 absolute">
+        <fieldset className="border border-black p-3 rounded my-3">
           <legend>Username</legend>
           <input type="text" name="signUpUsername" placeholder="maven123" required autoComplete="off" maxLength="15"/>
         </fieldset>
-        <fieldset className="border border-black p-3 rounded">
+        <fieldset className="border border-black p-3 rounded my-3">
           <legend>Name</legend>
           <input type="text" name="signUpName" placeholder="Carlos"/>
         </fieldset>
-        <fieldset className="border border-black p-3 rounded">
+        <fieldset className="border border-black p-3 rounded my-3">
           <legend>Last Name</legend>
           <input type="text" name="signUpLastName" placeholder="&Aacute;lvarez"/>
         </fieldset>
-        <fieldset className="border border-black p-3 rounded">
+        <fieldset className="border border-black p-3 rounded my-3">
           <legend>E-mail</legend>
           <input type="email" name="signUpEmail" placeholder="karthus@gmail.com" required autoComplete="off"/>
         </fieldset>
-        <fieldset className="border border-black p-3 rounded" hidden>
+        <fieldset className="border border-black p-3 rounded my-3" hidden>
           <legend>Confirm E-mail</legend>
           <input type="email" name="signUpEmailConfirmation" placeholder="karthus@gmail.com"  required autoComplete="off" />
         </fieldset>
-        <fieldset className="border border-black p-3 rounded">
+        <fieldset className="border border-black p-3 rounded my-3">
           <legend>Password</legend>
           <input type="password" name="signUpPassword" placeholder="1234" required autoComplete="off" minLength="8"/>
         </fieldset>
-        <fieldset className="border border-black p-3 rounded" hidden>
+        <fieldset className="border border-black p-3 rounded my-3" hidden>
           <legend>Confirm Password</legend>
           <input type="password" name="signUpPasswordConfirmation" placeholder="1234" required autoComplete="off" minLength="8"/>
         </fieldset>
         <span className="inline my-4 min-w-fit">
-          <button className="border-2 rounded p-2 bg-green-500" type="submit">Confirm</button>
-          <Link to="/"> <button className="border-2 rounded p-2 bg-red-500">Cancel</button> </Link>
+          <button className="border-2 rounded-2xl p-2 bg-green-300 hover:bg-green-500" type="submit">Confirm</button>
+          <Link to="/"> <button className="border-2 rounded-2xl p-2 bg-red-300 hover:bg-red-500">Cancel</button> </Link>
         </span>
-      </form>
-    </div>
+     </div>
+    </form>
   )
 
 }
